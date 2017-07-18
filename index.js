3c4100cd62cb80421e1ec76d40ce17232cf6005c
sxc <295058820@qq.com>
2017-07-18 17:44:25 +0800

    vvv

---
+++ index.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var app = express();
+var bodyparser = require('body-parser');
+
+var fs = require('fs');//文件管理系统
+var multer = require('multer');
+var form = multer();
+//app.use(bodyparser.text());
+app.use(express.static('public'));
+app.use(bodyparser.urlencoded({extended:true}));
+
+app.post('/zc',function(req,res){
+
+var pwd = req.body.pwd;
+var name = req.body.username;
+console.log(name)
+console.log(pwd)
+res.send({'name':name, 
+           'pwd':pwd})
+})
+
+
+
+
+
+
+
+
+
+app.listen(3000,function(){
+console.log('服务器启动成功！')	
+	
+	
+	
+})
